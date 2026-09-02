644a15af535d109f39c0a6216d1d7a5218cea968
TheCanadian0417 <hhancharuk@gmail.com>
2026-09-02 16:23:03 -0400

    Fix assets path

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import FollowUpTracker from "./FollowUpTracker.jsx";
+import BugTracker from "./BugTracker.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FollowUpTracker />
+    <BugTracker />
   </StrictMode>,
 )
